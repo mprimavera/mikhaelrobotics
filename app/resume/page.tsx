968c3dfa367823a0efa5e9490493df0400870bfa
Michael Primavera <mr.michael.primavera@outlook.com>
2025-12-19 16:46:36 -0500

    just cleaned up the resume page. Ready for deployment

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -25,10 +25,10 @@ export default function Resume() {
       content: (
         <div>
           <p className="mb-2 sm:mb-6 font-normal text-slate-300  sm:break-normal max-w-full text-lg sm:text-3xl font-semibold leading-10 tracking-tight">
-            Applying for M.S. in Artificial Intelligence (online), Johns Hopkins University, starting May, 2026
+            Applying for M.S. in Artificial Intelligence (online), Johns Hopkins University, starting May 2026
           </p>
           <a href="https://ep.jhu.edu/programs/artificial-intelligence/"
-            target="blank"
+            target="_blank"  rel="noopener noreferrer"
             className="mb-8 font-normal text-blue-500 hover:text-blue-400  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
             Artificial Intelligence Master's Program Online
           </a>
@@ -51,7 +51,12 @@ export default function Resume() {
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
             I custom built this portfolio completely from scratch (no templates or tutorials). I used GitHub, VSCode, NEXT.JS with React and
-             route handling, TailwindCSS, Aceternity UI, ShadCN, Lucide, and ChatGPT. I purchased a domain and will use Vercel to host the site. 
+             route handling, TailwindCSS, Aceternity UI, ShadCN, Lucide, and ChatGPT.
+        </p>
+
+        <p 
+          className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
+            I purchased a domain and will use Vercel to host the site. 
         </p>
       {/* </div> */}
       
@@ -84,7 +89,7 @@ export default function Resume() {
             icon={
               <Image
                 src="/react.png"
-                alt="React"
+                alt="React logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -96,9 +101,9 @@ export default function Resume() {
 
           <GridItem2
             icon={
-                    <Image
+               <Image
                 src="/tailwindCSS.png"
-                alt="React"
+                alt="Tailwindcss logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -110,9 +115,9 @@ export default function Resume() {
 
           <GridItem2
             icon={
-                    <Image
+              <Image
                 src="/aceternityUI.png"
-                alt="React"
+                alt="Aceternity logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -124,9 +129,9 @@ export default function Resume() {
 
           <GridItem2
             icon={
-                    <Image
+              <Image
                 src="/shadcn.png"
-                alt="React"
+                alt="Shadcn Logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -138,9 +143,9 @@ export default function Resume() {
 
           <GridItem2
             icon={
-                    <Image
+              <Image
                 src="/lucide1.png"
-                alt="React"
+                alt="Lucide logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -154,7 +159,7 @@ export default function Resume() {
             icon={
                     <Image
                 src="/chatgpt1.png"
-                alt="React"
+                alt="ChatGPT logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -168,7 +173,7 @@ export default function Resume() {
             icon={
                     <Image
                 src="/vercel2.png"
-                alt="React"
+                alt="Vercel logo"
                 width={24}
                 height={24}
                 className="object-contain"
@@ -177,14 +182,8 @@ export default function Resume() {
             description="Vercel"
             link="https://vercel.com/home"
           />
-
-
         </div>
-
       </div>
-
-
-
         
     </div>
   ),
@@ -203,7 +202,7 @@ export default function Resume() {
       {/* <div className="mb-8 max-h-40 sm:max-h-40 overflow-y-auto pr-2"> */}
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
-            I ...
+            I will be receiving my diploma in early January, 2026. I officially finished my courses on 12/12/2025.
         </p>
       {/* </div> */}
     </div>
@@ -223,11 +222,16 @@ export default function Resume() {
       <div className="mb-8 max-h-40 sm:max-h-40 overflow-y-auto pr-2">
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
-            I began working at Milara in their machine shop. Less than six months later I had been promoted to system integration on robotics
+            I began working at Milara in their machine shop. Less than six months later I had been promoted to system integration on robotic
             sorters in the semiconductor industry. Three months later, I was promoted to field service engineering as a member of the software team.
-            After Milara offered to pay my tuition to earn my degree, I applied to WPI and got accepted. I had to make a major life decision, return to
-            college or stay at the job I had secured. I chose to earn the degree, and I have zero regret. 
         </p>
+
+        <p 
+          className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
+            After Milara offered to pay my tuition to earn my degree, I applied to WPI and got accepted. I had to make a major life decision, return to
+            college or stay at the job I had secured. It was one of the best decisions I've ever made.
+        </p>
+        
       </div>
     </div>
   ),
@@ -243,12 +247,54 @@ export default function Resume() {
       </p>
 
       {/* Scrollable container */}
-      {/* <div className="mb-8 max-h-40 sm:max-h-40 overflow-y-auto pr-2"> */}
+      <div className="mb-8 max-h-40 sm:max-h-40 overflow-y-auto pr-2">
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
-            Building off previous experience in logistics management, I ...
+            Building off previous experience in logistics management, I decided to manufacture products and sell them direct to customers.
+            I formed an LLC, and dealt with everything from developing price modeling, to product photos and web development, to customer service
+            and setting up automated shipping nexuses in each state.
         </p>
-      {/* </div> */}
+
+        <p 
+          className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
+            Although I did make sales, the income wasn't steady enough to pursue it further.
+        </p>
+
+
+      </div>
+             
+      <Card className="w-full relative flex flex-col md:flex-row items-center md:items-start gap-4 p-4 ">
+        <GlowingEffect
+          spread={40}
+          glow={true}
+          disabled={false}
+          proximity={64}
+          inactiveZone={0.01}
+        />
+
+        <CardContent className="w-full md:w-1/2 flex justify-center">
+          <div className="w-full aspect-video">
+            <iframe
+              className="w-full h-full rounded-lg"
+              src="https://www.youtube.com/embed/ImnPgaXRYiE?si=rXmw1jVXKugv35bx"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+
+        </CardContent>
+
+        <CardHeader className="w-full md:w-1/2">
+          <CardTitle>I developed dynamic price modeling using multivariable calculus and MATLAB</CardTitle>
+          <CardDescription>
+          I learned through real-life experience what determines whether a company survives: coming up with a mathematical
+          model of a proper go-to-market strategy for profitability.
+          </CardDescription>
+        </CardHeader>
+      </Card>
     </div>
   ),
 },
@@ -266,10 +312,16 @@ export default function Resume() {
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
             Utilizing my skills in manufacturing, and combining those with creativity and an intuitive understanding of market demand, I developed
-            three simple products, all of which sold. It was an amazing experience to come up with an idea in my mind, then a few months after 
-            meticulous planning for market delivery and profitability, I was holding in my hand a tangible version of a thumb screw design which
-            had previously existed only within my mind.
+            three simple products, all of which sold.
         </p>
+
+        <p 
+          className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
+            It was an amazing experience to come up with an idea in my mind, then a few months after meticulous planning for market delivery and
+            profitability, I was holding in my hand a tangible version of a thumb screw design which had previously existed only within my mind.
+        </p>
+
+
       </div>
     </div>
   ),
@@ -292,8 +344,8 @@ export default function Resume() {
           first time "investing", and it ultimately led to me taking five online courses, listening to over a dozen audiobooks, reading
           a few more books, then watching every microeconomic, macroeconomic, finance, capital markets, and accounting video on Khan Academy.
         </p>
-              </div>
-        {/* <Card className="w-full  flex items-center justify-center"> */}
+        </div>
+
       <Card className="w-full relative flex flex-col md:flex-row items-center md:items-start gap-4 p-4 ">
         <GlowingEffect
           spread={40}
@@ -303,7 +355,6 @@ export default function Resume() {
           inactiveZone={0.01}
         />
 
-        {/* Video on the left */}
         <CardContent className="w-full md:w-1/2 flex justify-center">
           <div className="w-full aspect-video">
             <iframe
@@ -318,13 +369,12 @@ export default function Resume() {
           </div>
         </CardContent>
 
-        {/* Description on the right */}
         <CardHeader className="w-full md:w-1/2">
-          <CardTitle>The first prototype for AlphaOmega™</CardTitle>
+          <CardTitle>The first prototype for AlphaOmega LLC</CardTitle>
           <CardDescription>
-            In the process of learning to invest, I had built an Ethereum mining rig for about $4,000 USD. At the time, they were
-          selling on Etsy for $10,000 to $20,000. I had inadverntatnly created the first prototype that would lead to the launch of
-          AlphaOmega™ in 2018.
+          In the process of learning to invest, I had built an Ethereum mining rig for about $4,000 USD. At the time, they were
+          selling on Etsy for $10,000 to $20,000. I had inadvertently created the first prototype that would lead to the launch of
+          AlphaOmega LLC in 2018.
           </CardDescription>
         </CardHeader>
       </Card>
@@ -340,15 +390,15 @@ export default function Resume() {
   content: (
     <div>
       <p className="mb-2 sm:mb-6 text-slate-300  sm:break-normal max-w-full text-lg sm:text-3xl font-semibold leading-10 tracking-tight">
-        Copyright on Mind: Part I of a trillogy
+        Copyright on Mind: Part I of a trilogy
       </p>
 
       {/* Scrollable container */}
       {/* <div className="mb-8 max-h-40 sm:max-h-40 overflow-y-auto pr-2"> */}
         <p 
           className="mb-2 text-slate-500  sm:break-normal max-w-full sm:text-2xl leading-10 tracking-tight">
-          In October of 2017 I had finished the final draft of part one on a trillogy that I wrote. I was determined to see it through to completion,
-          and even though I never intended to become published, I hired an intellectual attourney to aid in securing my first piece of IP.
+          In October of 2017 I had finished the final draft of part one on a trilogy that I wrote. I was determined to see it through to completion,
+          and even though I never intended to become published, I hired an intellectual attorney to aid in securing my first piece of IP.
         </p>
 
         <GridItem
@@ -357,7 +407,7 @@ export default function Resume() {
           title={
             <a
               href="https://publicrecords.copyright.gov/detailed-record/voyager_29808827"
-              target="blank"
+              target="_blank"  rel="noopener noreferrer"
               className="text-blue-500 text-center  sm:break-normal hover:text-blue-400">
               Copyright for Mind
             </a>
